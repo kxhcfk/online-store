16ffb1a12cb8bf8248e4932578b346fd2942c794
kxhcfk <reveersed@mail.ru>
2023-09-21 20:15:38 +0300

    feat: i18n

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,13 +5,17 @@ import {Header} from "@/widgets/Header";
 import {RouterProvider} from "@/app/providers/RouterProvider";
 import {useTheme} from "@/shares/theme";
 import {classNames} from "@/shares/lib/classNames";
+import {useTranslation} from "react-i18next";
 
 const App = () => {
     const {theme} = useTheme();
+    const {t} = useTranslation();
     
     return (
         <div className={classNames("app", theme)}>
             <Header/>
+            
+            {t("test")}
             
             <main>
                 <RouterProvider/>
--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import {StrictMode} from "react";
 import {BrowserRouter} from "react-router-dom";
 import {ThemeProvider} from "@/app/providers/ThemeProvider";
 
+import '@/shares/config/i18n';
+
 const root = createRoot(document.getElementById("root"));
 
 root.render(
